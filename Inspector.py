6ee0cf4dc9bd7e965e108d75b9f256bd1a6080a2
Abdullahalshra3a <39274124+Abdullahalshra3a@users.noreply.github.com>
2020-08-28 11:11:13 +0200

    Update Inspector.py

--- Inspector.py
+++ Inspector.py
@@ -19,26 +19,26 @@ from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.packet import packet
-from ryu.lib.packet import ethernet
+from ryu.lib.packet import ethernet, ipv4, ipv6 , arp, icmp
 from ryu.lib.packet import ether_types
 from ryu.lib.packet import tcp
-import pickle
+import pickle, threading
+import socket
 
 
 class SimpleSwitch13(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
-    Hostnumber = 15
+    Hostnumber = 123
     Hostinfo ={}
     def __init__(self, *args, **kwargs):
         super(SimpleSwitch13, self).__init__(*args, **kwargs)
         self.mac_to_port = {}
-        self. switch = {}
+
     @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
     def switch_features_handler(self, ev):
         datapath = ev.msg.datapath
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
-
         # install table-miss flow entry
         #
         # We specify NO BUFFER to max_len of the output action due to
@@ -68,7 +68,9 @@ class SimpleSwitch13(app_manager.RyuApp):
             mod = parser.OFPFlowMod(datapath=datapath, priority=priority,
                                     match=match, instructions=inst)
         datapath.send_msg(mod)
+   
 
+    
     @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
     def _packet_in_handler(self, ev):
         # If you hit this you might want to increase
@@ -85,33 +87,37 @@ class SimpleSwitch13(app_manager.RyuApp):
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
         pkt_tcp = pkt.get_protocol(tcp.tcp)
+        ip = pkt.get_protocol(ipv4.ipv4)
+
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:
             # ignore lldp packet
             return
         dst = eth.dst
         src = eth.src
-        if pkt_tcp:# dpid in edge switches
-           print pkt
-           print "pak:", pkt_tcp.seq
-        dpid = format(datapath.id, "d").zfill(16)
+
+        dpid = datapath.id
         self.mac_to_port.setdefault(dpid, {})
 
         self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
         
         if ip:
           if src in self.Hostinfo.keys():
-               s = pickle.dumps(ip.src,in_port,dpid, src,dst)
-               self.sendToC1(self, s)
+               Tuple = (ip.src,in_port,dpid, src,dst,msg.buffer_id, msg.data)
+               y = pickle.dumps(Tuple)
+               self.sendToC1(y)
           elif len(self.Hostinfo) >= self.Hostnumber :
             print "BLOCK"
             self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
             actions = []
             match = parser.OFPMatch(in_port=in_port)
-            self.add_flow(datapath, 100, match, in_port , actions)
+            self.add_flow(datapath, 100, match, in_port , actions)#should be 1000
             return
           else:
             print "new item"
             self.Hostinfo[src] = (ip.src,in_port,dpid)
+            Tuple = (ip.src,in_port,dpid, src,dst,msg.buffer_id, msg.data)
+            y = pickle.dumps(Tuple)
+            self.sendToC1(y)
 
     
 
@@ -119,9 +125,8 @@ class SimpleSwitch13(app_manager.RyuApp):
     def sendToC1(self, y):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         host = socket.gethostname() # Get local machine name
-        port = 12345                # Reserve a port for your service.
+        port = 5001                # Reserve a port for your service.
         s.connect((host, port))
-        data = jsonpickle.encode(y)
-        s.sendall(data)
+        s.sendall(y)
         #print s.recv(1024)
         s.close
